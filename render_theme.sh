e3d4931cfb267509a2fdf2bae01ead45dc2769b8
Walter Huf <hufman@gmail.com>
2018-11-20 21:04:16 -0800

    Skips rerendering output files unnecessarily
    Checks based on mtime of output files vs source files

--- render_theme.sh
+++ render_theme.sh
@@ -23,10 +23,12 @@ find . -name '*.svg' | while read path; do
     
     if grep -q "xlink:href.*bmp" "$path"; then
       bmppath=$(dirname "$path")/"$layername".bmp
+      [ "$bmppath" -nt "$path" ] && continue	# skip files that are newer
       #convert -background '#910101' -transparent-color '#910101' <(hide_layers "$path" "$layername") gif:"$bmppath".gif
       size=$(xmlstarlet sel -N svg="http://www.w3.org/2000/svg" -t -v "/svg:svg/@width" -o 'x' -v "/svg:svg/@height" "$path")
       convert -verbose -density 90 -background '#010105' <(hide_layers "$path" "$layername") -transparent-color '#010105' gif:- | convert gif:- -transparent '#010105' -background cyan -flatten -alpha off -compress none -type Palette -colors 256 -gravity center -crop "$size+0+0" BMP3:"$bmppath"
     elif grep -q "xlink:href.*png" "$path"; then
+      [ "$pngpath" -nt "$path" ] && continue	# skip files that are newer
       pngpath=$(dirname "$path")/"$layername".png
       convert <(hide_layers "$path" "$layername") "$pngpath"
     fi
