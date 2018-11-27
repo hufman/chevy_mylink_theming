0b654fdd1abb2812661cd70dbe41ad30d54bd11e
Walter Huf <hufman@gmail.com>
2018-11-27 00:28:03 -0800

    Renders pngs better

--- render_theme.sh
+++ render_theme.sh
@@ -14,6 +14,7 @@ function hide_layers {
 find . -name '*.svg' | while read path; do
   grep -q "StillImage" "$path" || continue
 
+  size=$(xmlstarlet sel -N svg="http://www.w3.org/2000/svg" -t -v "/svg:svg/@width" -o 'x' -v "/svg:svg/@height" "$path")
   layers=$(cat "$path" | xmlstarlet sel \
     -N svg="http://www.w3.org/2000/svg" \
     -t -v "/svg:svg/svg:g[starts-with(@id, 'New_')]/@id"
@@ -25,12 +26,12 @@ find . -name '*.svg' | while read path; do
       bmppath=$(dirname "$path")/"$layername".bmp
       [ "$bmppath" -nt "$path" ] && continue	# skip files that are newer
       #convert -background '#910101' -transparent-color '#910101' <(hide_layers "$path" "$layername") gif:"$bmppath".gif
-      size=$(xmlstarlet sel -N svg="http://www.w3.org/2000/svg" -t -v "/svg:svg/@width" -o 'x' -v "/svg:svg/@height" "$path")
-      convert -verbose -density 90 -background '#010105' <(hide_layers "$path" "$layername") -transparent-color '#010105' gif:- | convert gif:- -transparent '#010105' -background cyan -flatten -alpha off -compress none -type Palette -colors 256 -gravity center -crop "$size+0+0" BMP3:"$bmppath"
+      convert -verbose -density 96 -background '#010105' <(hide_layers "$path" "$layername") -transparent-color '#010105' gif:- | convert gif:- -transparent '#010105' -background cyan -flatten -alpha off -compress none -type Palette -colors 256 -gravity center -crop "$size+0+0" BMP3:"$bmppath"
     elif grep -q "xlink:href.*png" "$path"; then
-      [ "$pngpath" -nt "$path" ] && continue	# skip files that are newer
       pngpath=$(dirname "$path")/"$layername".png
-      convert <(hide_layers "$path" "$layername") "$pngpath"
+      [ "$pngpath" -nt "$path" ] && continue	# skip files that are newer
+      #convert -verbose -density 96 <(hide_layers "$path" "$layername") -gravity center -crop "$size+0+0" "$pngpath"
+      inkscape <(hide_layers "$path" "$layername") --export-png="$pngpath" --export-area-page --export-dpi='96,96' --export-background-opacity='0.0'
     fi
   done
 done
